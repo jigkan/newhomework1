d9f4410d629e59f8a240f616134713dc63639a5c
jigkan <jigneshkananibpharm@gmail.com>
2023-04-27 23:17:15 +0100

    try 2

--- newhomework1/src/Try1.java
+++ newhomework1/src/Try1.java
@@ -1,7 +1,11 @@
 public class Try1 {
 
     public static void main (String[]args)
-    { System.out.println("hellow try1"); }
+
+    { System.out.println("hellow try1");
+    System.out.println("hellow try2");
+
+    }
 
 
 }
